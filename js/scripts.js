eaa3960cdf5be1a55964391e3e29bb218b646c8e
nuriyyaruslan <nuriyya.isgandarova@gmail.com>
2024-12-24 15:46:54 +0400

    done home

--- js/scripts.js
+++ js/scripts.js
@@ -64,5 +64,14 @@ $(function() {
     $('.filters-open').removeClass('hidden');
   })
 
+  //HEADER SCROLL ACTIVE
+  $(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("active");
+    }
+});
 
   });
